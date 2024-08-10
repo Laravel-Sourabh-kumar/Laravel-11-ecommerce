3a79cdac15b35d35b7eb4b93ad86dd5b35a74db2
sourabhkumar12345 <kumariee1998@gmail.com>
2024-08-10 12:51:25 +0530

    first commit

---
+++ app/Livewire/Skeleton/ItemSkeleton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Skeleton;
+
+use Livewire\Component;
+
+class ItemSkeleton extends Component
+{
+    public function render()
+    {
+        return view('livewire.skeleton.item-skeleton');
+    }
+}
